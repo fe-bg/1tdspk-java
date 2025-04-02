8a9365d4e78d2a0c40e98f88279b792b3be74d9b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-02 12:01:46 -0300

    aula 02/04

--- CadastroPessoa/src/br/com/fiap/cadastro/model/Endereco.java
+++ CadastroPessoa/src/br/com/fiap/cadastro/model/Endereco.java
@@ -2,7 +2,7 @@ package br.com.fiap.cadastro.model;
 
 public class Endereco {
 
-    String logradouro;
+    private String logradouro;
     private String numero;
     private String bairro;
     private String municipio;
@@ -12,7 +12,7 @@ public class Endereco {
 
     public Endereco() {
     }
-
+    //construtor aq
     public Endereco(String logradouro, String numero, String complemento, String bairro,
                     String municipio, String uf, String cep) {
         this.bairro = bairro;
@@ -24,5 +24,62 @@ public class Endereco {
         this.uf = uf;
     }
 
+    //metodos acessores
 
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 }
